ff4f1110de4bef60a76fa42eaf3282143d1a1709
PaataSet <paataseturidze@gmail.com>
2024-12-08 21:55:53 +0400

    add some code
    add some code for experimental use

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,128 +7,128 @@ const slides = document.querySelectorAll(".slide");
 const prevSlideBtn = document.querySelector("#prev-slide");
 const nextSlideBtn = document.querySelector("#next-slide");
 const syncJsFns = () => {
-	const logInfo = () => {
-		console.log("logInfo");
-	};
+  const logInfo = () => {
+    console.log("logInfo");
+  };
 
-	function logInfoFn() {
-		console.log("logInfoFn");
-	}
+  function logInfoFn() {
+    console.log("logInfoFn");
+  }
 
-	const syncFn = () => {
-		console.log("function start");
-		for (let i = 0; i < 1000; i++) {
-			console.log(i);
-		}
-		console.log("function end");
-	};
+  const syncFn = () => {
+    console.log("function start");
+    for (let i = 0; i < 1000; i++) {
+      console.log(i);
+    }
+    console.log("function end");
+  };
 
-	const asyncFn = () => {
-		console.log("async function start");
+  const asyncFn = () => {
+    console.log("async function start");
 
-		setTimeout(() => {
-			console.log("setTimeout");
+    setTimeout(() => {
+      console.log("setTimeout");
 
-			// modal.classList.remove("active");
-		}, 2000);
+      // modal.classList.remove("active");
+    }, 2000);
 
-		console.log("async function after setTimeout");
-	};
+    console.log("async function after setTimeout");
+  };
 
-	// syncFn();
-	// asyncFn();
+  // syncFn();
+  // asyncFn();
 
-	// setInterval(logInfo, 2000)
-	let intervalId = null,
-		timeoutId = null;
+  // setInterval(logInfo, 2000)
+  let intervalId = null,
+    timeoutId = null;
 
-	startIntervalBtn.addEventListener("click", () => {
-		intervalId = setInterval(logInfo, 2000);
-	});
+  startIntervalBtn.addEventListener("click", () => {
+    intervalId = setInterval(logInfo, 2000);
+  });
 
-	stopIntervalBtn.addEventListener("click", () => {
-		if (intervalId) {
-			clearInterval(intervalId);
-			intervalId = null;
-		}
+  stopIntervalBtn.addEventListener("click", () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
 
-		console.log("clear interval");
-	});
+    console.log("clear interval");
+  });
 
-	startTimeoutBtn.addEventListener("click", () => {
-		timeoutId = setTimeout(logInfo, 5000);
-		console.log("timeoutId", timeoutId);
-	});
+  startTimeoutBtn.addEventListener("click", () => {
+    timeoutId = setTimeout(logInfo, 5000);
+    console.log("timeoutId", timeoutId);
+  });
 
-	stopTimeoutBtn.addEventListener("click", () => {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-			timeoutId = null;
-		}
-		console.log("clear timeout");
-	});
+  stopTimeoutBtn.addEventListener("click", () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    }
+    console.log("clear timeout");
+  });
 };
 
 const slideFn = () => {
-	let currentSlide = 2;
-	// slides[currentSlide].classList.add("active");
+  let currentSlide = 2;
+  // slides[currentSlide].classList.add("active");
 
-	const showSlides = () => {
-		slides.forEach((slide, index) => {
-			if (index === currentSlide) {
-				slide.classList.add("active");
-			} else {
-				slide.classList.remove("active");
-			}
-		});
-	};
+  const showSlides = () => {
+    slides.forEach((slide, index) => {
+      if (index === currentSlide) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+  };
 
-	const goToPrevSlide = () => {
-		if (currentSlide === 0) {
-			// prevSlideBtn.style.pointerEvents = "none";
-			// prevSlideBtn.disabled = true;
-			currentSlide = slides.length - 1;
-		} else {
-			// prevSlideBtn.style.pointerEvents = "initial";
-			// prevSlideBtn.disabled = false;
-			currentSlide--;
-		}
+  const goToPrevSlide = () => {
+    if (currentSlide === 0) {
+      // prevSlideBtn.style.pointerEvents = "none";
+      // prevSlideBtn.disabled = true;
+      currentSlide = slides.length - 1;
+    } else {
+      // prevSlideBtn.style.pointerEvents = "initial";
+      // prevSlideBtn.disabled = false;
+      currentSlide--;
+    }
 
-		// console.log(currentSlide);
-		showSlides();
-	};
+    // console.log(currentSlide);
+    showSlides();
+  };
 
-	const goToNextSlide = () => {
-		if (currentSlide === slides.length - 1) {
-			// nextSlideBtn.style.pointerEvents = "none";
-			// nextSlideBtn.disabled = true;
-			currentSlide = 0;
-		} else {
-			currentSlide++;
-		}
+  const goToNextSlide = () => {
+    if (currentSlide === slides.length - 1) {
+      // nextSlideBtn.style.pointerEvents = "none";
+      // nextSlideBtn.disabled = true;
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
 
-		// console.log(currentSlide);
-		showSlides();
-	};
+    // console.log(currentSlide);
+    showSlides();
+  };
 
-	prevSlideBtn.addEventListener("click", goToPrevSlide);
-	nextSlideBtn.addEventListener("click", goToNextSlide);
+  // prevSlideBtn.addEventListener("click", goToPrevSlide);
+  // nextSlideBtn.addEventListener("click", goToNextSlide);
 
-	// const slideInterval = setInterval(goToNextSlide, 3000);
-	// clearInterval(slideInterval);
+  // const slideInterval = setInterval(goToNextSlide, 3000);
+  // clearInterval(slideInterval);
 
-	// document.addEventListener("keyup", (event) => {
-	// 	console.log(event);
+  // document.addEventListener("keyup", (event) => {
+  // 	console.log(event);
 
-	// 	if (event.code === "ArrowLeft") {
-	// 		goToPrevSlide();
-	// 	}
-	// 	if (event.code === "ArrowRight") {
-	// 		goToNextSlide();
-	// 	}
-	// });
+  // 	if (event.code === "ArrowLeft") {
+  // 		goToPrevSlide();
+  // 	}
+  // 	if (event.code === "ArrowRight") {
+  // 		goToNextSlide();
+  // 	}
+  // });
 
-	showSlides();
+  showSlides();
 };
 
 slideFn();
--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,15 @@ function showTime() {
 
 showTime();
 
-/*
+// მეორე ვერსია
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+  document.getElementById("clock2").innerHTML = date.toLocaleTimeString();
+}
+
+/* 
 2. ლექციაზე შექმნილ სლაიდერს დავამატოთ: 
    2.1. დავამატოთ სლაიდების ავტომატური ცვლილება 5 წამიანი ინტერვალით 
    2.2. როდესაც ავტომატურად ხდება სლაიდების შეცვლა თუ მაუსს მივიტან სურათთან, ავტომატური სლაიდი გაჩერდეს.
@@ -46,7 +54,36 @@ showTime();
    mouseenter, mouseleave  ივენთებს 
    დამხმარე მასალა: https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseenter_event  
 https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseleave_event
+*/
 
+/*
  3*(optional) დავამატოთ მარტივი countdown რომელიც გვიჩვენებს მომდევნო ლექციამდე (3 დეკემბერი, 20:00) დარჩენილ 
  დროს (დღე, საათი, წუთი)
  */
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 10, 2024 20:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
